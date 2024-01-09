c2314bc4786496165b4d83fbf4411e7b0cd0cee0
Jean-Pierre Portier <141755467+JPPortier@users.noreply.github.com>
2024-01-09 14:21:04 +0100

    DEVEXP-264: validate E164 number format (#37)
    * feat (DEVEXP-264): E164 format validation

--- client/src/main/com/sinch/sdk/models/E164PhoneNumber.java
+++ client/src/main/com/sinch/sdk/models/E164PhoneNumber.java
@@ -1,11 +1,35 @@
 package com.sinch.sdk.models;
 
 import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class E164PhoneNumber {
+
+  // See https://community.sinch.com/t5/Glossary/E-164/ta-p/7537
+  private static final Pattern PATTERN =
+      // adapted from https://stackoverflow.com/a/40347281
+      Pattern.compile(
+          String.format(
+              "%s%s%s%s",
+              // prefix
+              "^\\+",
+              // 1 to 3 digits: Country code (with or without parentheses) / Do not accept '0' as
+              // 1st digit
+              // https://en.wikipedia.org/wiki/List_of_country_calling_codes
+              "(([\\\\(][1-9][0-9]{0,2}[\\\\)])|([1-9][0-9]{0,2}))",
+              // Minimum of 5 digits (for fixed line phones in Solomon Islands)
+              "\\d\\d\\d\\d\\d",
+              // ending with digits
+              "\\d*$"));
+
   private final String number;
 
   public E164PhoneNumber(String number) {
+
+    if (!validate(number)) {
+      throw new NumberFormatException(String.format("Invalid E164 format for '%s' number", number));
+    }
     this.number = number;
   }
 
@@ -15,6 +39,12 @@ public class E164PhoneNumber {
 
   public static E164PhoneNumber valueOf(String value) {
     return new E164PhoneNumber(value);
+  }
+
+  public static boolean validate(String value) {
+    // don't worry about spaces
+    Matcher matcher = PATTERN.matcher(value.replaceAll("\\s+", ""));
+    return matcher.matches();
   }
 
   @Override
---
+++ client/src/test/java/com/sinch/sdk/models/E164PhoneNumberTest.java
@@ -0,0 +1,62 @@
+package com.sinch.sdk.models;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+import java.util.Collection;
+import org.junit.jupiter.api.Test;
+
+class E164PhoneNumberTest {
+
+  String phoneNumber = "+123456789";
+  Collection<String> validNumbers =
+      Arrays.asList(
+          phoneNumber,
+          "+11234567890",
+          "+(1)1234567890",
+          "+4407123456789",
+          "+461234567890",
+          "+13172222222",
+          "+1447851987654",
+          "+1 4478519876",
+          "+33628254417",
+          "+(33)628254417",
+          "+(33) 628254417",
+          "+33 628254417",
+          "+6773771234");
+
+  Collection<String> invalidNumbers =
+      Arrays.asList("text value", "1447851987654", "+", "+1123", "+11234", "+03172222222");
+
+  @Test
+  void stringValue() {
+    assertEquals(new E164PhoneNumber(phoneNumber).stringValue(), phoneNumber);
+  }
+
+  @Test
+  void valueOf() {
+    assertEquals(E164PhoneNumber.valueOf(phoneNumber), new E164PhoneNumber(phoneNumber));
+  }
+
+  @Test
+  void throwExceptionOnInvalid() {
+    String value = "my invalid value";
+    Exception exception =
+        assertThrows(NumberFormatException.class, () -> E164PhoneNumber.valueOf(value));
+    assertTrue(
+        exception.getMessage().contains(value),
+        String.format("'%s' should contains '%s'", exception.getMessage(), value));
+  }
+
+  @Test
+  void validateTrue() {
+    validNumbers.forEach(
+        f -> assertTrue(E164PhoneNumber.validate(f), String.format("'%s' is valid", f)));
+  }
+
+  @Test
+  void validateFalse() {
+    invalidNumbers.forEach(
+        f -> assertFalse(E164PhoneNumber.validate(f), String.format("'%s' is invalid", f)));
+  }
+}
