0601b8a4579b162572250d64023dc3b53c277468
Jean-Pierre Portier <jean-pierre.portier@mailgun.com>
2024-05-22 11:49:22 +0200

    refactor (samples): Synch with new status classes in place of report

--- sample-app/src/main/java/com/sinch/sample/verification/status/GetById.java
+++ sample-app/src/main/java/com/sinch/sample/verification/status/GetById.java
@@ -2,7 +2,7 @@ package com.sinch.sample.verification.status;
 
 import com.sinch.sample.BaseApplication;
 import com.sinch.sdk.domains.verification.models.VerificationId;
-import com.sinch.sdk.domains.verification.models.VerificationReport;
+import com.sinch.sdk.domains.verification.models.VerificationStatus;
 import java.io.IOException;
 import java.util.logging.Logger;
 
@@ -23,11 +23,11 @@ public class GetById extends BaseApplication {
 
   public void run() {
 
-    VerificationId id = VerificationId.valueOf(callId);
+    VerificationId id = VerificationId.valueOf(verificationId);
 
     LOGGER.info("Get status by id for  : " + id);
 
-    VerificationReport response = client.verification().verificationStatus().getById(id);
+    VerificationStatus response = client.verification().verificationStatus().getById(id);
     LOGGER.info("Response :" + response);
   }
 }
