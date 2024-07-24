a42fea8f1e66f0add531902022a929caf41a36fd
Jean-Pierre Portier <141755467+JPPortier@users.noreply.github.com>
2024-07-24 09:17:12 +0200

    Merge pull request #107 from sinch/feat/DEVEXP-492-numbers-webhook-validateAuthenticationHeader
    DEVEXP-492: Numbers webhook validate authentication header

--- client/src/test/java/com/sinch/sdk/domains/numbers/api/v1/adapters/WebhooksServiceTest.java
+++ client/src/test/java/com/sinch/sdk/domains/numbers/api/v1/adapters/WebhooksServiceTest.java
@@ -1,5 +1,7 @@
 package com.sinch.sdk.domains.numbers.api.v1.adapters;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import com.adelean.inject.resources.junit.jupiter.GivenTextResource;
 import com.adelean.inject.resources.junit.jupiter.TestWithResources;
 import com.sinch.sdk.BaseTest;
@@ -11,6 +13,10 @@ import com.sinch.sdk.domains.numbers.models.v1.WebhooksDtoTest;
 import com.sinch.sdk.domains.numbers.models.v1.webhooks.NumberEvent;
 import com.sinch.sdk.models.Configuration;
 import java.io.IOException;
+import java.util.AbstractMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -21,6 +27,26 @@ public class WebhooksServiceTest extends BaseTest {
   String incomingWebhookJSON;
 
   WebHooksService webHooksService;
+
+  @Test
+  void checkValidateAuthenticatedRequest() {
+
+    Map<String, String> headers =
+        Stream.of(
+                new AbstractMap.SimpleEntry<>(
+                    NumbersWebhooksAuthenticationValidation.SIGNATURE_HEADER,
+                    "e289b8fac44491417aeceee28d2715840b5f1aca"))
+            .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+    String secret = "secret value";
+    String body =
+        "{\"eventId\":\"01j3fycq0fyqq3gkgxb0g5ekbz\",\"timestamp\":\"2024-07-23T14:02:14.415637289\",\"projectId\":\"b8bae3c6-e7df-422b-9401-8ae3ee3e0aff\",\"resourceId\":\"+12053017799\",\"resourceType\":\"ACTIVE_NUMBER\",\"eventType\":\"PROVISIONING_TO_VOICE_PLATFORM\",\"status\":\"SUCCEEDED\",\"failureCode\":null}";
+
+    boolean authenticationResult =
+        webHooksService.validateAuthenticationHeader(secret, headers, body);
+
+    assertTrue(authenticationResult);
+  }
 
   @Test
   void parse() throws ApiException {
