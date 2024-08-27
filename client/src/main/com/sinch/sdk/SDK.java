15fd76b5ca62acebfe44de8fafbdd6fe4df7f2f6
git <github-actions@github.com>
2024-08-27 08:02:57 +0000

    build (release): Bump version to 3.3.3 for sources

--- client/src/main/com/sinch/sdk/SDK.java
+++ client/src/main/com/sinch/sdk/SDK.java
@@ -3,6 +3,6 @@ package com.sinch.sdk;
 public class SDK {
 
   public static final String NAME = "Sinch Java SDK";
-  public static final String VERSION = "1.3.0-SNAPSHOT";
+  public static final String VERSION = "3.3.3";
   public static final String AUXILIARY_FLAG = "";
 }
