2100c3e6ef3d78570b5569adfca25a2e50a22c18
git <github-actions@github.com>
2024-08-27 08:05:36 +0000

    build (release): Set next version to 4.4.4-dev for sources

--- client/src/main/com/sinch/sdk/SDK.java
+++ client/src/main/com/sinch/sdk/SDK.java
@@ -3,6 +3,6 @@ package com.sinch.sdk;
 public class SDK {
 
   public static final String NAME = "Sinch Java SDK";
-  public static final String VERSION = "3.3.3";
+  public static final String VERSION = "4.4.4-dev";
   public static final String AUXILIARY_FLAG = "";
 }
