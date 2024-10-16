154859a5013d447e36c634832227b5eaafc5cf78
Jean-Pierre Portier <jean-pierre.portier@mailgun.com>
2024-10-16 07:50:52 +0200

    docs (Voice): javadoc update

--- client/src/main/com/sinch/sdk/domains/voice/models/v1/destination/DestinationConference.java
+++ client/src/main/com/sinch/sdk/domains/voice/models/v1/destination/DestinationConference.java
@@ -1,3 +1,11 @@
 package com.sinch.sdk.domains.voice.models.v1.destination;
 
+import com.sinch.sdk.domains.voice.models.v1.callouts.request.CalloutRequestConference;
+
+/**
+ * Base class related to a <code>Conference</code> call destination
+ *
+ * @see com.sinch.sdk.domains.voice.api.v1.CalloutsService#conference(CalloutRequestConference)
+ * @since 1.4
+ */
 public interface DestinationConference {}
--- client/src/main/com/sinch/sdk/domains/voice/models/v1/destination/DestinationCustom.java
+++ client/src/main/com/sinch/sdk/domains/voice/models/v1/destination/DestinationCustom.java
@@ -1,3 +1,11 @@
 package com.sinch.sdk.domains.voice.models.v1.destination;
 
+import com.sinch.sdk.domains.voice.models.v1.callouts.request.CalloutRequestCustom;
+
+/**
+ * Base class related to a <code>Custom</code> call destination
+ *
+ * @see com.sinch.sdk.domains.voice.api.v1.CalloutsService#custom(CalloutRequestCustom)
+ * @since 1.4
+ */
 public interface DestinationCustom {}
