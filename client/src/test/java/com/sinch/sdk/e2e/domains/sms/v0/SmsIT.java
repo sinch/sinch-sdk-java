530910da3bc65906a7926b991e13e102ee26297f
Jean-Pierre Portier <jean-pierre.portier@mailgun.com>
2024-11-28 16:23:39 +0100

    test (SMS/E2E): Batches

---
+++ client/src/test/java/com/sinch/sdk/e2e/domains/sms/v0/SmsIT.java
@@ -0,0 +1,16 @@
+package com.sinch.sdk.e2e.domains.sms.v0;
+
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.SuiteDisplayName;
+
+@Suite
+@SuiteDisplayName("SMS V0")
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features/sms/batches.feature")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.sinch.sdk.e2e.domains.sms.v0")
+public class SmsIT {}
