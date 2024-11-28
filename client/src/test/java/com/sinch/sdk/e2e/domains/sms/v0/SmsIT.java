2764467a4b12b8d085bf008ecc0a9319136b4295
Jean-Pierre Portier <jean-pierre.portier@mailgun.com>
2024-11-28 17:45:10 +0100

    test (SMS/E2E): Delivery reports

--- client/src/test/java/com/sinch/sdk/e2e/domains/sms/v0/SmsIT.java
+++ client/src/test/java/com/sinch/sdk/e2e/domains/sms/v0/SmsIT.java
@@ -12,5 +12,6 @@ import org.junit.platform.suite.api.SuiteDisplayName;
 @SuiteDisplayName("SMS V0")
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features/sms/batches.feature")
+@SelectClasspathResource("features/sms/delivery-reports.feature")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.sinch.sdk.e2e.domains.sms.v0")
 public class SmsIT {}
