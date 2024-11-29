14d13abda6f33cb981134ec852eff908e67feeb9
Jean-Pierre Portier <jean-pierre.portier@mailgun.com>
2024-11-29 09:31:50 +0100

    test (SMS/E2E): Webhooks

--- client/src/test/java/com/sinch/sdk/e2e/domains/sms/v0/SmsIT.java
+++ client/src/test/java/com/sinch/sdk/e2e/domains/sms/v0/SmsIT.java
@@ -11,9 +11,6 @@ import org.junit.platform.suite.api.SuiteDisplayName;
 @Suite
 @SuiteDisplayName("SMS V0")
 @IncludeEngines("cucumber")
-@SelectClasspathResource("features/sms/batches.feature")
-@SelectClasspathResource("features/sms/delivery-reports.feature")
-@SelectClasspathResource("features/sms/groups.feature")
-@SelectClasspathResource("features/sms/inbounds.feature")
+@SelectClasspathResource("features/sms")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.sinch.sdk.e2e.domains.sms.v0")
 public class SmsIT {}
