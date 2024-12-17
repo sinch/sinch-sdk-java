b7b1d79ff06436d429dd2dec3eca6e227c95ec12
Jean-Pierre Portier <jean-pierre.portier@mailgun.com>
2024-12-17 08:53:20 +0100

    test (SMS): Extend SMS service init unit tests coverage

--- client/src/test/java/com/sinch/sdk/domains/sms/api/v1/adapters/SMSServiceTest.java
+++ client/src/test/java/com/sinch/sdk/domains/sms/api/v1/adapters/SMSServiceTest.java
@@ -1,11 +1,13 @@
 package com.sinch.sdk.domains.sms.api.v1.adapters;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.sinch.sdk.core.http.HttpClient;
 import com.sinch.sdk.core.models.ServerConfiguration;
 import com.sinch.sdk.models.SmsContext;
+import com.sinch.sdk.models.SmsServicePlanCredentials;
 import com.sinch.sdk.models.UnifiedCredentials;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
@@ -15,11 +17,11 @@ class SMSServiceTest {
   @Mock HttpClient httpClient;
 
   @Test
-  void doNotAcceptNullKey() {
+  void projectIdDoNotAcceptNullKey() {
     UnifiedCredentials credentials =
         UnifiedCredentials.builder().setKeyId(null).setKeySecret("foo").setProjectId("foo").build();
-    SmsContext context = SmsContext.builder().build();
-    ServerConfiguration server = new ServerConfiguration("");
+    SmsContext context = SmsContext.builder().setSmsUrl("https://sms.foo.url").build();
+    ServerConfiguration server = new ServerConfiguration("https://oauth.foo.url");
     Exception exception =
         assertThrows(
             IllegalArgumentException.class,
@@ -28,11 +30,11 @@ class SMSServiceTest {
   }
 
   @Test
-  void doNotAcceptNullKeySecret() {
+  void projectIdDoNotAcceptNullKeySecret() {
     UnifiedCredentials credentials =
         UnifiedCredentials.builder().setKeyId("foo").setKeySecret(null).setProjectId("foo").build();
-    SmsContext context = SmsContext.builder().build();
-    ServerConfiguration server = new ServerConfiguration("");
+    SmsContext context = SmsContext.builder().setSmsUrl("https://sms.foo.url").build();
+    ServerConfiguration server = new ServerConfiguration("https://oauth.foo.url");
     Exception exception =
         assertThrows(
             IllegalArgumentException.class,
@@ -41,11 +43,11 @@ class SMSServiceTest {
   }
 
   @Test
-  void doNotAcceptNullProject() {
+  void projectIdDoNotAcceptNullProject() {
     UnifiedCredentials credentials =
         UnifiedCredentials.builder().setKeyId("foo").setKeySecret("foo").setProjectId(null).build();
-    SmsContext context = SmsContext.builder().build();
-    ServerConfiguration server = new ServerConfiguration("");
+    SmsContext context = SmsContext.builder().setSmsUrl("https://sms.foo.url").build();
+    ServerConfiguration server = new ServerConfiguration("https://oauth.foo.url");
 
     Exception exception =
         assertThrows(
@@ -55,10 +57,10 @@ class SMSServiceTest {
   }
 
   @Test
-  void doNotAcceptNullCredentials() {
+  void projectIdDoNotAcceptNullCredentials() {
 
-    SmsContext context = SmsContext.builder().build();
-    ServerConfiguration server = new ServerConfiguration("");
+    SmsContext context = SmsContext.builder().setSmsUrl("https://sms.foo.url").build();
+    ServerConfiguration server = new ServerConfiguration("https://oauth.foo.url");
     Exception exception =
         assertThrows(
             NullPointerException.class, () -> new SMSService(null, context, server, httpClient));
@@ -66,18 +68,122 @@ class SMSServiceTest {
   }
 
   @Test
-  void doNotAcceptNullContext() {
+  void projectIdDoNotAcceptNullContext() {
     UnifiedCredentials credentials =
         UnifiedCredentials.builder()
             .setKeyId("foo")
             .setKeySecret("foo")
             .setProjectId("foo")
             .build();
-    ServerConfiguration server = new ServerConfiguration("");
+    ServerConfiguration server = new ServerConfiguration("https://oauth.foo.url");
     Exception exception =
         assertThrows(
             NullPointerException.class,
             () -> new SMSService(credentials, null, server, httpClient));
     assertTrue(exception.getMessage().contains("Context must be defined"));
+  }
+
+  @Test
+  void projectIdDoNotAcceptNullSmsUrl() {
+    UnifiedCredentials credentials =
+        UnifiedCredentials.builder()
+            .setKeyId("foo")
+            .setKeySecret("foo")
+            .setProjectId("foo")
+            .build();
+    SmsContext context = SmsContext.builder().setSmsUrl(null).build();
+    ServerConfiguration server = new ServerConfiguration("https://oauth.foo.url");
+    Exception exception =
+        assertThrows(
+            IllegalArgumentException.class,
+            () -> new SMSService(credentials, context, server, httpClient));
+    assertTrue(exception.getMessage().contains("smsUrl"));
+  }
+
+  @Test
+  void projectIdUsagePassed() {
+
+    UnifiedCredentials credentials =
+        UnifiedCredentials.builder()
+            .setKeyId("foo key ")
+            .setKeySecret("foo secret")
+            .setProjectId("foo project")
+            .build();
+    SmsContext context = SmsContext.builder().setSmsUrl("https://sms.foo.url").build();
+    ServerConfiguration server = new ServerConfiguration("https://oauth.foo.url");
+
+    assertDoesNotThrow(
+        () -> new SMSService(credentials, context, server, httpClient), "Init passed");
+  }
+
+  @Test
+  void servicePlanIdDoNotAcceptNullApiToken() {
+    SmsServicePlanCredentials credentials =
+        SmsServicePlanCredentials.builder().setApiToken(null).setServicePlanId("foo plan").build();
+    SmsContext context = SmsContext.builder().setSmsUrl("https://sms.foo.url").build();
+    Exception exception =
+        assertThrows(
+            IllegalArgumentException.class, () -> new SMSService(credentials, context, httpClient));
+    assertTrue(exception.getMessage().contains("apiToken"));
+  }
+
+  @Test
+  void servicePlanIdDoNotAcceptNullServicePlanId() {
+    SmsServicePlanCredentials credentials =
+        SmsServicePlanCredentials.builder().setApiToken("foo token").setServicePlanId(null).build();
+    SmsContext context = SmsContext.builder().setSmsUrl("https://sms.foo.url").build();
+    Exception exception =
+        assertThrows(
+            IllegalArgumentException.class, () -> new SMSService(credentials, context, httpClient));
+    assertTrue(exception.getMessage().contains("servicePlanId"));
+  }
+
+  @Test
+  void servicePlanIdDoNotAcceptNullCredentials() {
+
+    SmsContext context = SmsContext.builder().setSmsUrl("https://sms.foo.url").build();
+    Exception exception =
+        assertThrows(NullPointerException.class, () -> new SMSService(null, context, httpClient));
+    assertTrue(exception.getMessage().contains("Credentials must be defined"));
+  }
+
+  @Test
+  void servicePlanIdDoNotAcceptNullContext() {
+    SmsServicePlanCredentials credentials =
+        SmsServicePlanCredentials.builder()
+            .setApiToken("foo token")
+            .setServicePlanId("foo plan")
+            .build();
+    Exception exception =
+        assertThrows(
+            NullPointerException.class, () -> new SMSService(credentials, null, httpClient));
+    assertTrue(exception.getMessage().contains("Context must be defined"));
+  }
+
+  @Test
+  void servicePlanIdDoNotAcceptNullSmsUrl() {
+    SmsServicePlanCredentials credentials =
+        SmsServicePlanCredentials.builder()
+            .setApiToken("foo token")
+            .setServicePlanId("foo plan")
+            .build();
+    SmsContext context = SmsContext.builder().setSmsUrl(null).build();
+    Exception exception =
+        assertThrows(
+            IllegalArgumentException.class, () -> new SMSService(credentials, context, httpClient));
+    assertTrue(exception.getMessage().contains("smsUrl"));
+  }
+
+  @Test
+  void servicePlanIdUsagePassed() {
+
+    SmsServicePlanCredentials credentials =
+        SmsServicePlanCredentials.builder()
+            .setApiToken("foo token")
+            .setServicePlanId("foo plan")
+            .build();
+    SmsContext context = SmsContext.builder().setSmsUrl("https://sms.foo.url").build();
+
+    assertDoesNotThrow(() -> new SMSService(credentials, context, httpClient), "Init passed");
   }
 }
