22732979144ce33b5d3d81824f0dfdcefb7dbbc0
Jean-Pierre Portier <jean-pierre.portier@mailgun.com>
2025-07-21 13:17:09 +0200

    build: Bump to V2.0

--- client/src/main/com/sinch/sdk/SDK.java
+++ client/src/main/com/sinch/sdk/SDK.java
@@ -3,6 +3,6 @@ package com.sinch.sdk;
 public class SDK {
 
   public static final String NAME = "Sinch Java SDK";
-  public static final String VERSION = "1.8.0-dev";
+  public static final String VERSION = "2.0.0-dev";
   public static final String AUXILIARY_FLAG = "";
 }
