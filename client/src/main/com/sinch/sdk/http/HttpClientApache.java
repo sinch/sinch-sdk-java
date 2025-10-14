0d56535d5b7ca78f6b8e12f3cb31f9db72fc67f2
Antoine SEIN <142824551+asein-sinch@users.noreply.github.com>
2025-10-14 10:24:20 +0200

    Fix NPE when handling 401 response without headers (#274)

--- client/src/main/com/sinch/sdk/http/HttpClientApache.java
+++ client/src/main/com/sinch/sdk/http/HttpClientApache.java
@@ -200,7 +200,8 @@ public class HttpClientApache implements com.sinch.sdk.core.http.HttpClient {
     Map<String, List<String>> responseHeaders = response.getHeaders();
     Collection<String> header = responseHeaders.get(BEARER_AUTHENTICATE_RESPONSE_HEADER_KEYWORD);
 
-    boolean headerPresent = header.stream().anyMatch(e -> e.contains(BEARER_EXPIRED_KEYWORD));
+    boolean headerPresent =
+        header != null && header.stream().anyMatch(e -> e.contains(BEARER_EXPIRED_KEYWORD));
     if (headerPresent) {
       bearerAuthManager.resetToken();
     }
@@ -332,7 +333,7 @@ public class HttpClientApache implements com.sinch.sdk.core.http.HttpClient {
     }
   }
 
-  private HttpResponse processRequest(CloseableHttpClient client, ClassicHttpRequest request)
+  HttpResponse processRequest(CloseableHttpClient client, ClassicHttpRequest request)
       throws IOException {
     return client.execute(request, HttpClientApache::processResponse);
   }
