a566692a51064a5ae1fee20d9220ff679185737e
Jean-Pierre Portier <jean-pierre.portier@mailgun.com>
2026-02-09 10:33:49 +0100

    feat (exmaple/client): Add 'examples/client' template

--- examples/compile.sh
+++ examples/compile.sh
@@ -6,4 +6,5 @@ export SDK_VERSION="${SDK_VERSION}"
 
 (cd snippets && ./compile.sh)  || exit 1
 (cd webhooks && ./compile.sh)  || exit 1
+(cd client &&  mvn -Puse-version clean package)  || exit 1
 (cd getting-started && ./compile.sh)  || exit 1
