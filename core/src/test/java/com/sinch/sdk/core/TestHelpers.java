5876c1ed1c3fd42c209c8895acd4921d9a0a2f38
Jean-Pierre Portier <jean-pierre.portier@mailgun.com>
2026-02-26 04:00:25 +0100

    test: Simplified version of TestHelpers to ignore functionnal interface during comparison

--- core/src/test/java/com/sinch/sdk/core/TestHelpers.java
+++ core/src/test/java/com/sinch/sdk/core/TestHelpers.java
@@ -1,13 +1,9 @@
 package com.sinch.sdk.core;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
 import java.util.Arrays;
 import java.util.Iterator;
-import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
 import org.assertj.core.api.Assertions;
 import org.assertj.core.api.recursive.comparison.RecursiveComparisonConfiguration;
 
@@ -26,7 +22,7 @@ public class TestHelpers {
                 new String[][] {
                   excludedFields,
                   Arrays.stream(actual.getClass().getDeclaredFields())
-                      .filter(f -> isFunctionalInterface(f.getType()))
+                      .filter(f -> f.getType().isAnnotationPresent(FunctionalInterface.class))
                       .map(Field::getName)
                       .toArray(String[]::new)
                 })
@@ -42,17 +38,5 @@ public class TestHelpers {
     AtomicInteger count = new AtomicInteger();
     iterator.forEachRemaining(i -> count.incrementAndGet());
     Assertions.assertThat(count.get()).isEqualTo(expectedCount);
-  }
-
-  static boolean isFunctionalInterface(Class<?> iface) {
-    if (!iface.isInterface()) return false;
-    // Exclude Object methods (e.g., equals, hashCode)
-    List<Method> abstractMethods =
-        Arrays.stream(iface.getMethods())
-            .filter(m -> Modifier.isAbstract(m.getModifiers()))
-            .filter(m -> !m.getDeclaringClass().equals(Object.class))
-            .collect(Collectors.toList());
-
-    return abstractMethods.size() == 1;
   }
 }
