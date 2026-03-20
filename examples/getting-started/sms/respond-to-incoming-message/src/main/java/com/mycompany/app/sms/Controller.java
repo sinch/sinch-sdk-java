f4704882cb15535200adeb31869782005785c5eb
Jean-Pierre Portier <jean-pierre.portier@mailgun.com>
2026-03-20 08:00:25 +0100

    refactor (SMS/getting-started): Simplify 'respond-to-incoming-message' with Conversation one to remove sinch events validation

--- examples/getting-started/sms/respond-to-incoming-message/src/main/java/com/mycompany/app/sms/Controller.java
+++ examples/getting-started/sms/respond-to-incoming-message/src/main/java/com/mycompany/app/sms/Controller.java
@@ -4,26 +4,18 @@ import com.sinch.sdk.SinchClient;
 import com.sinch.sdk.domains.sms.api.v1.SinchEventsService;
 import com.sinch.sdk.domains.sms.models.v1.inbounds.TextMessage;
 import com.sinch.sdk.domains.sms.models.v1.sinchevents.SmsSinchEvent;
-import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 @RestController("SMS")
 public class Controller {
 
   private final SinchClient sinchClient;
   private final ServerBusinessLogic serverBusinessLogic;
-
-  @Value("${sms.sinchevents.secret: }")
-  private String sinchEventsSecret;
 
   @Autowired
   public Controller(SinchClient sinchClient, ServerBusinessLogic serverBusinessLogic) {
@@ -35,31 +27,9 @@ public class Controller {
       value = "/SmsSinchEvent",
       consumes = MediaType.APPLICATION_JSON_VALUE,
       produces = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity<Void> smsDeliveryEvent(
-      @RequestHeader Map<String, String> headers, @RequestBody String body) {
+  public ResponseEntity<Void> smsDeliveryEvent(@RequestBody String body) {
 
     SinchEventsService sinchEvents = sinchClient.sms().v1().sinchEvents();
-
-    // ensure valid authentication to handle request
-    // See
-    // https://developers.sinch.com/docs/sms/api-reference/sms/tag/Webhooks/#tag/Webhooks/section/Callbacks
-    // Contact your account manager to configure your callback sending headers validation
-    // set "ensureValidAuthentication" value to true to validate requests from Sinch servers
-    boolean ensureValidAuthentication = false;
-    if (ensureValidAuthentication) {
-      var validAuth =
-          sinchEvents.validateAuthenticationHeader(
-              sinchEventsSecret,
-              // request headers
-              headers,
-              // request payload body
-              body);
-
-      // token validation failed
-      if (!validAuth) {
-        throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
-      }
-    }
 
     // decode the request payload
     SmsSinchEvent event = sinchEvents.parseEvent(body);
