ce0ef9ff8905ce0dc5a84953655b53422f8bf24b
Jean-Pierre Portier <jean-pierre.portier@mailgun.com>
2026-03-26 13:57:53 +0100

    feat (SMS/lazyinit): Use 'Supplier' for double checking and lazy init

--- client/src/main/com/sinch/sdk/domains/sms/adapters/SMSService.java
+++ client/src/main/com/sinch/sdk/domains/sms/adapters/SMSService.java
@@ -9,31 +9,40 @@ import java.util.function.Supplier;
 
 public class SMSService implements com.sinch.sdk.domains.sms.SMSService {
 
-  private final com.sinch.sdk.domains.sms.api.v1.SMSService v1;
+  private final Supplier<com.sinch.sdk.domains.sms.api.v1.SMSService> v1Initializer;
+
+  private volatile com.sinch.sdk.domains.sms.api.v1.SMSService v1;
 
   public SMSService(
       UnifiedCredentials credentials,
       SmsContext context,
       ServerConfiguration oAuthServer,
       Supplier<HttpClient> httpClientSupplier) {
-
-    this.v1 =
-        new com.sinch.sdk.domains.sms.api.v1.adapters.SMSService(
-            credentials, context, oAuthServer, httpClientSupplier);
+    this.v1Initializer =
+        () ->
+            new com.sinch.sdk.domains.sms.api.v1.adapters.SMSService(
+                credentials, context, oAuthServer, httpClientSupplier);
   }
 
   public SMSService(
       SmsServicePlanCredentials credentials,
       SmsContext context,
       Supplier<HttpClient> httpClientSupplier) {
-
-    this.v1 =
-        new com.sinch.sdk.domains.sms.api.v1.adapters.SMSService(
-            credentials, context, httpClientSupplier);
+    this.v1Initializer =
+        () ->
+            new com.sinch.sdk.domains.sms.api.v1.adapters.SMSService(
+                credentials, context, httpClientSupplier);
   }
 
   @Override
   public com.sinch.sdk.domains.sms.api.v1.SMSService v1() {
+    if (null == this.v1) {
+      synchronized (this) {
+        if (null == this.v1) {
+          this.v1 = v1Initializer.get();
+        }
+      }
+    }
     return this.v1;
   }
 }
