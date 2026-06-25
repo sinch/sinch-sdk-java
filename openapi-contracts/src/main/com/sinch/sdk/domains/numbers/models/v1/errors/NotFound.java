231d3063a934e57abfa7a55c34383ad54dc38e16
Eduardo San Segundo <investing.edu.bh@gmail.com>
2026-06-25 17:53:06 +0200

    New generated Java Number sources and test coverage for internalFailureCode field

--- openapi-contracts/src/main/com/sinch/sdk/domains/numbers/models/v1/errors/NotFound.java
+++ openapi-contracts/src/main/com/sinch/sdk/domains/numbers/models/v1/errors/NotFound.java
@@ -23,6 +23,7 @@ public interface NotFound {
 
   /** Gets or Sets code */
   public class CodeEnum extends EnumDynamic<Integer, CodeEnum> {
+    /** The HTTP status code for a resource not found. */
     public static final CodeEnum NUMBER_404 = new CodeEnum(404);
 
     private static final EnumSupportDynamic<Integer, CodeEnum> ENUM_SUPPORT =
@@ -61,6 +62,7 @@ public interface NotFound {
 
   /** Gets or Sets status */
   public class StatusEnum extends EnumDynamic<String, StatusEnum> {
+    /** The request resource could not be found. */
     public static final StatusEnum NOT_FOUND = new StatusEnum("NOT_FOUND");
 
     private static final EnumSupportDynamic<String, StatusEnum> ENUM_SUPPORT =
