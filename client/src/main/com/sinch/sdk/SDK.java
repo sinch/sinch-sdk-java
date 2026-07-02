b8d58bc3cd6ca137f3a0bb1aa9dbad32421b683d
git <github-actions@github.com>
2026-07-02 14:11:31 +0000

    build (release): Bump version to 2.1.0 for sources

--- client/src/main/com/sinch/sdk/SDK.java
+++ client/src/main/com/sinch/sdk/SDK.java
@@ -3,6 +3,6 @@ package com.sinch.sdk;
 public class SDK {
 
   public static final String NAME = "Sinch Java SDK";
-  public static final String VERSION = "2.1.0-dev";
+  public static final String VERSION = "2.1.0";
   public static final String AUXILIARY_FLAG = "";
 }
