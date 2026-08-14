8abe3f3b5466c8754496a052f3f3078a2864c68e
Eduardo San Segundo <investing.edu.bh@gmail.com>
2026-08-14 17:35:05 +0200

    Removed empty empty E2E tests

--- client/src/test/java/com/sinch/sdk/e2e/domains/numbers/v1/SinchEventsSteps.java
+++ client/src/test/java/com/sinch/sdk/e2e/domains/numbers/v1/SinchEventsSteps.java
@@ -26,9 +26,6 @@ public class SinchEventsSteps {
 
   static final String WEBHOOKS_PATH = Config.NUMBERS_HOST_NAME + "/webhooks/numbers/";
   static final String SECRET = "strongPa$$PhraseWith36CharactersMax";
-
-  static final String UNSUPPORTED_EVENT_TYPE = "NUMBER_ORDER_PROCESSING";
-
   SinchEventsService service;
 
   Map<String, String> triggerToURL =
@@ -55,10 +52,6 @@ public class SinchEventsSteps {
   @When("I send a request to trigger the {string} for {string} event")
   public void triggerEvent(String status, String trigger) throws IOException {
 
-    if (UNSUPPORTED_EVENT_TYPE.equals(trigger)) {
-      return;
-    }
-
     WebhooksHelper.Response<NumberSinchEvent> response =
         WebhooksHelper.callURL(
             new URL(triggerToURL.get(status + "_" + trigger)), service::parseEvent);
@@ -67,10 +60,6 @@ public class SinchEventsSteps {
 
   @Then("the header of the {string} for {string} event contains a valid signature")
   public void validateEventSignature(String status, String trigger) {
-
-    if (UNSUPPORTED_EVENT_TYPE.equals(trigger)) {
-      return;
-    }
 
     WebhooksHelper.Response<NumberSinchEvent> receivedEvent =
         receivedEvents.get(status + "_" + trigger);
@@ -83,10 +72,6 @@ public class SinchEventsSteps {
 
   @Then("the event describes a {string} for {string} event")
   public void validateResult(String status, String trigger) {
-
-    if (UNSUPPORTED_EVENT_TYPE.equals(trigger)) {
-      return;
-    }
 
     NumberSinchEvent expectedSuccess =
         NumberSinchEvent.builder()
